0e1b52a5f1b25904eb51853aa00211710b54f859
Andrei Karpushonak <karpushonak@gmail.com>
2012-12-14 15:54:39 +0100

    Added spies on existing methods

--- sinon.js
+++ sinon.js
@@ -4,18 +4,24 @@ var should = require('should')
 var assert = require('assert')
 
 var Person = {
-  sayHi: function() {
-    return "hi"
+  sayHi: function(saying) {
+    return "hey, " + saying
   }
 }
+
+describe('print', function() {
+  it('just printing', function() {
+   
+    Person.sayHi('dude').should.equal('hey, dude')
+     console.log(Person.sayHi('dude'))
+  })
+})
 
 describe('Person', function() {
   describe('#sayHi', function() {
     it('says hola', sinon.test(function() {
-      this.stub(Person, 'sayHi').returns('hola')
-      var said = Person.sayHi()
-      console.log(said)
-      Person.sayHi().should.equal('hola')
+      this.stub(Person, 'sayHi').returns('hola')     
+      Person.sayHi('Andrei').should.equal('hola')
     }))
 
     it('says bonjour', sinon.test(function() {
@@ -25,16 +31,28 @@ describe('Person', function() {
   })
 })
 
-describe('spy', function() {
-  it('count invocations sayHi', function() {
+describe('spy on existing method', function() {
+  it('shall check argument', sinon.test(function() {
 
-    Person.sayHi = sinon.spy()
-    Person.sayHi()
+    sinon.spy(Person, 'sayHi')
+    var said = Person.sayHi('again')
+    console.log(said)
 
     assert(Person.sayHi.called)
+    // alternatives
     assert(Person.sayHi.calledOnce)
     Person.sayHi.calledTwice.should.equal(false)
     Person.sayHi.callCount.should.equal(1)
-    
-  })
+
+    Person.sayHi('Andrei')
+
+    // check args for first and second call
+    Person.sayHi.getCall(0).args[0].should.equal('again')
+    Person.sayHi.getCall(1).args[0].should.equal('Andrei')
+
+    // remove spy
+    Person.sayHi.restore()
+
+  }))
 })
+
