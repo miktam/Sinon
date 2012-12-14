bff2bf3d86c80243a706f4353abdb11e06399bb9
Andrei Karpushonak <karpushonak@gmail.com>
2012-12-14 14:48:26 +0100

    Added spies

--- sinon.js
+++ sinon.js
@@ -1,27 +1,40 @@
-var sinon = require('sinon');
+var sinon = require('sinon')
+var mocha = require('mocha')
+var should = require('should')
+var assert = require('assert')
 
-var mocha = require('mocha');
-var should = require('should');
-var sinon = require('sinon');
- 
-var Person = global.Person = {
+var Person = {
   sayHi: function() {
-    return "hi";
+    return "hi"
   }
 }
- 
+
 describe('Person', function() {
   describe('#sayHi', function() {
     it('says hola', sinon.test(function() {
-      this.stub(Person, 'sayHi').returns('hola');
-      var said = Person.sayHi();
+      this.stub(Person, 'sayHi').returns('hola')
+      var said = Person.sayHi()
       console.log(said)
-      Person.sayHi().should.equal('hola');    
+      Person.sayHi().should.equal('hola')
     }))
-    
+
     it('says bonjour', sinon.test(function() {
-      this.stub(Person, 'sayHi').returns('bonjour');
-      Person.sayHi().should.equal('bonjour');
+      this.stub(Person, 'sayHi').returns('bonjour')
+      Person.sayHi().should.equal('bonjour')
     }))
+  })
+})
+
+describe('spy', function() {
+  it('count invocations sayHi', function() {
+
+    Person.sayHi = sinon.spy()
+    Person.sayHi()
+
+    assert(Person.sayHi.called)
+    assert(Person.sayHi.calledOnce)
+    Person.sayHi.calledTwice.should.equal(false)
+    Person.sayHi.callCount.should.equal(1)
+    
   })
 })
